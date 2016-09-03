c036c056f134599ba7c02a4687ddad049f5d4299
Chris <chris@codereviewvideos.com>
2016-09-03 11:26:03 +0100

    removed EightPoints Guzzle from AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
-            new EightPoints\Bundle\GuzzleBundle\GuzzleBundle(),
-
             new AppBundle\AppBundle(),
         ];
 
